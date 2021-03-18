a3e71cc6f406acfa831c6b3bc0a090c30b235d42
gathumann <63115755+gathumann@users.noreply.github.com>
2021-03-17 20:58:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,22 @@ def fibonacci(fibInput):
 @app.route('/is-prime/<primeInput>')
 def prime(primeInput):
     return "prime"
+# is-prime program
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+	
+	for i in range (2, int(num/2)+1):
+		
+		if (num % i) == 0:
+			print(num, "is not a prime number")
+			break
+	else:
+		print (num, "is a prime number")
+			
+	else:
+		print(num, "is not a prime number"
 
 @app.route('/slack-alert/<slackInput>')
 def slack(slackInput):
