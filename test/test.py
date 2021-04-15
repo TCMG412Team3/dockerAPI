40e57bdcaab824d571fff990945282324c77e531
Braxton Williams <braxtonwill@gmail.com>
2021-04-14 21:35:56 -0500

    Added beginning of test application

---
+++ test/test.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+import argparse
+
+def testEndpoint(endpointURL, requestMethod, expectedStatus, expectedResponse):
+    print("Testing endpoint: '{}' with method {}".format(endpointURL, requestMethod))
+    response = requests.request(requestMethod, url=endpointURL)
+    if response.status_code in expectedStatus and (response.text == expectedResponse or expectedResponse == None):
+        print("Test PASSED\n")
+        return 0
+    print("Test FAILED")
+    print("Expected status:{}\nRecieved status:{}".format(expectedStatus, response.status_code))
+    print("Expected response:{}\nRecieved response:{}".format(expectedResponse, response.text))
+    global failedCount
+    failedCount = failedCount + 1
+    return 1
+
+def testEndpointJSON(url, method, requestData, expectedStatus, expectedResponse):
+    return 0
+
+failedCount = 0
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', dest='HOSTNAME', default='localhost', help='Specify the hostname for the API (default: localhost)')
+parser.add_argument('--port', dest='PORT', default='5000', help='Specify the port on the API host (default: 5000)')
+args = parser.parse_args()
+
+
+hostname = args.HOSTNAME
+port = args.PORT
+
+apiURL = 'http://{}:{}'.format(hostname, port)
+
+
+try:
+    request = requests.get(apiURL)
+except:
+    print("Error contacting API, please ensure that hostname and port are correct.")
+    sys.exit(1)
+
+
+testEndpoint(apiURL + "/md5", 'GET', [400,404,405], None)
